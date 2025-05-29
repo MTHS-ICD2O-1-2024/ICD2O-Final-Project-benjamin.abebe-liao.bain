83fbfd7df65c79543c859e9c282b1e48ebdfe4e3
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-29 17:22:13 +0000

    made changes on the html and js files

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 /* global Phaser */
 
-// Created by: Benjamin Abebe and Bain Liao
-// Created on: June 2025
+// Created by: Benjamin Abebe
+// Created on: Apr 2025
 // This is the Phaser3 game configuration file
 
 // scene import statements
@@ -15,3 +15,39 @@ const splashScene = new SplashScene()
 const titleScene = new TitleScene()
 const menuScene = new MenuScene()
 const gameScene = new GameScene()
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
+
+// load scenes
+// Note: remember any "key" is global and CAN NOT be reused!
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
+game.scene.add('menuScene', menuScene)
+game.scene.add('gameScene', gameScene)
+
+// the start scene
+game.scene.start('splashScene')
+
--- js/titleScene.js
+++ js/titleScene.js
@@ -3,3 +3,54 @@
 // Created by: Benjamin Abebe and Bain Liao
 // Created on: June 2025
 // This is the Phaser3 game configuration file
+
+// clase for the title scene
+class TitleScene extends Phaser.Scene {
+  constructor() {
+    super({key: 'titleScene' })
+
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = {
+      font: '200px Times',
+      fill: '#fde4b9',
+      align: 'center'
+    }
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   *  before preload () and create ().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to load assets.
+   */
+  preload() {
+    console.log('Title Scene')
+    this.load.image('titleSceneBackground', 'assets/aliens_screen_image.jpg')
+  }
+
+  
+  create(data) {
+    this.titleSceneBackgroundImage = this.add.sprite(0, 0, 'titleSceneBackground').setScale(2.75)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2
+
+    this.titleSceneText = this.add.text(1920 / 2, 1080 / 2 + 350, 'Space Aliens', this.titleSceneTextStyle).setOrigin(0.5)
+  }
+
+  update(time, delta) {
+    if (time > 6000) {
+      this.scene.switch('menuScene')
+    }
+  }
+}
+
+export default TitleScene
